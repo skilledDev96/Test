abf94db2cb4ee6bd3419ca85aec39821690b6082
Ruan <rhdev96@gmail.com>
2024-02-07 12:05:12 +0200

    Added Details Page
    New Control Flow

--- test/src/app/details/details.page.ts
+++ test/src/app/details/details.page.ts
@@ -1,18 +1,42 @@
-import { Component, OnInit } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
-import { IonicModule } from '@ionic/angular';
+import { Component, OnInit, WritableSignal, inject, Input, signal } from '@angular/core';
+import { CommonModule, CurrencyPipe, DatePipe } from '@angular/common';
+import { MovieService } from '../services/movie.service';
+import { MovieResult } from '../services/interfaces';
+import { cashOutline, calendarOutline } from 'ionicons/icons'
+import { addIcons } from 'ionicons';
+import { IonHeader, IonToolbar, IonTitle, IonContent, InfiniteScrollCustomEvent,IonList,IonItem, IonSkeletonText,
+  IonAvatar,IonAlert,IonLabel,IonBadge,IonInfiniteScroll,IonInfiniteScrollContent,IonCard,IonCardHeader,IonCardSubtitle,
+  IonCardTitle,IonText, IonCardContent, IonIcon } from '@ionic/angular/standalone';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-details',
   templateUrl: './details.page.html',
   styleUrls: ['./details.page.scss'],
   standalone: true,
-  imports: [IonicModule, CommonModule, FormsModule]
+  imports: [IonHeader, IonToolbar, IonTitle, IonContent,IonList, IonItem, IonSkeletonText, 
+    IonAvatar,IonAlert,IonLabel, DatePipe, RouterModule, IonBadge,IonInfiniteScroll,IonInfiniteScrollContent, CurrencyPipe, DatePipe,IonCard,
+    IonCardHeader,IonCardSubtitle,IonCardTitle,IonText,IonCardContent,IonIcon],
 })
-export class DetailsPage implements OnInit {
 
-  constructor() { }
+export class DetailsPage implements OnInit {
+  private movieService = inject(MovieService)
+  public imageBaseUrl = 'https://image.tmdb.org/t/p'
+  public movie: WritableSignal<MovieResult | null> = signal(null)
+
+  @Input()
+  set id(movieId: string){
+    this.movieService.getMovieDetails(movieId).subscribe((movie) => {
+      console.log('movie',movie)
+
+      this.movie.set(movie)
+    })
+  }
+
+
+  constructor() {
+    addIcons({ cashOutline, calendarOutline})
+   }
 
   ngOnInit() {
   }
